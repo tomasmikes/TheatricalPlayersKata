58192af72a52831727498db77c7fdfe85f81aa00
Tomáš Mikeš <mikestomas.99@gmail.com>
2024-03-11 16:28:59 +0100

    Add CalculatePrice method

--- src/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ src/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -52,6 +52,9 @@ namespace TheatricalPlayersRefactoringKata
                 default:
                     throw new Exception("unknown type: " + play.Type);
             }
+
+            return price;
         }
+        
     }
 }
